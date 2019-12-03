146af0023f806ba3c8efec2f8c27398af9804582
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-12-03 17:01:11 +0100

    new ui

---
+++ precache-manifest.9b6288207eef96c4c41b9eaaf08f1f89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63188d0fd08eae292098bd11642532a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f05548dd77a4503ed03c",
+    "url": "/static/css/main.eb737fc3.chunk.css"
+  },
+  {
+    "revision": "68eb978e668ac1e29f8d",
+    "url": "/static/js/2.234c43c8.chunk.js"
+  },
+  {
+    "revision": "f05548dd77a4503ed03c",
+    "url": "/static/js/main.4a6bb7dd.chunk.js"
+  },
+  {
+    "revision": "b608db69e21ad2dde190",
+    "url": "/static/js/runtime-main.8ae06437.js"
+  }
+]);
