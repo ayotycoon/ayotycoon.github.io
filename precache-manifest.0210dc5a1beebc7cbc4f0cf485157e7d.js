d20eea11d1fc9a4699cc23866dd941b1acf8f7d2
ayotycoon <38573905+ayotycoon@users.noreply.github.com>
2019-12-04 13:23:57 +0100

    v3

---
+++ precache-manifest.0210dc5a1beebc7cbc4f0cf485157e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8938e37dea69e1736acb60318bb30f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "761c21a8eda3af679577",
+    "url": "/static/css/main.fd6adb92.chunk.css"
+  },
+  {
+    "revision": "68eb978e668ac1e29f8d",
+    "url": "/static/js/2.234c43c8.chunk.js"
+  },
+  {
+    "revision": "761c21a8eda3af679577",
+    "url": "/static/js/main.98cd5aa1.chunk.js"
+  },
+  {
+    "revision": "b608db69e21ad2dde190",
+    "url": "/static/js/runtime-main.8ae06437.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3ef1f1df66b7b182941c4afd00c914c4.js"
+  "/precache-manifest.0210dc5a1beebc7cbc4f0cf485157e7d.js"
 );
 
 self.addEventListener('message', (event) => {
